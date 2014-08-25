3d0b9b552edf56c4daa4ea0d086b2b1f6e8556c2
Brandon Madsen <bjackmadsen@gmail.com>
2014-08-25 10:27:27 -0700

    display the name of a station

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -6,4 +6,9 @@ describe Station do
 		station = Station.new('Alewife')
 		expect(station).to be_an_instance_of Station
 	end
+
+	it 'should display the station name' do
+		station = Station.new('Alewife')
+		expect(station.name).to eq 'Alewife'
+	end
 end
