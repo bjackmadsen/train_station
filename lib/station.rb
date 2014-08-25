a61d5f0994ef48dbf083803fab4a2ba6cb0e3e6d
Brandon Madsen <bjackmadsen@gmail.com>
2014-08-25 11:37:12 -0700

    add a station name to the database

--- lib/station.rb
+++ lib/station.rb
@@ -1,3 +1,5 @@
+require 'pg'
+
 class Station
 	def initialize(name)
 		@name = name
@@ -8,6 +10,18 @@ class Station
 	end
 
 	def self.all
-		[]
+		stations = []
+		database = PG.connect({:dbname => 'trainstation_test'})
+		results = database.exec("SELECT * FROM station;")
+		results.each do |result|
+			name = result ['name']
+			stations << Station.new(name)
+		end
+		stations
+	end
+
+	def save
+		database = PG.connect({:dbname => 'trainstation_test'})
+		database.exec("INSERT INTO station (name) VALUES ('#{@name}');")
 	end
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,5 +1,14 @@
 require 'station'
 require 'rspec'
+require 'pg'
+
+DB = PG.connect({:dbname => 'trainstation_test'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM station *;")
+    end
+end
 
 describe Station do 
 	it 'should initialize with a name' do 
@@ -15,5 +24,11 @@ describe Station do
 	it 'should start with no train stations' do
 		station = Station.new('Alewife')
 		expect(Station.all).to eq []
+	end
+
+	it 'should save train stations to the database' do
+		station = Station.new('Alewife')
+		station.save
+		expect(Station.all).to eq [station]
 	end
 end
