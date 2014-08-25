10bd315c29b4f6808b7d42aed9ecbe67b0c4f29d
Brandon Madsen <bjackmadsen@gmail.com>
2014-08-25 10:16:33 -0700

    initialize the station class with a name

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,7 +2,7 @@ require 'station'
 require 'rspec'
 
 describe Station do 
-	'it should initialize with a name' do 
+	it 'should initialize with a name' do 
 		station = Station.new('Alewife')
 		expect(station).to be_an_instance_of Station
 	end
