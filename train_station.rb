ca089a91cfa944bb762d233a13a5b102c3effade
Brandon Madsen <bjackmadsen@gmail.com>
2014-08-28 17:13:58 -0700

    add a new train line

--- train_station.rb
+++ train_station.rb
@@ -20,6 +20,7 @@ def welcome
 		exit
 	else
 		puts "Sorry, that wasn't a valid option."
+		sleep(1.0)
 		welcome 
 	end
 end
@@ -48,9 +49,108 @@ def operator_menu
 		welcome 
 	else
 		puts "Sorry, that wasn't a valid option."
-		welcome 
+		sleep(1.0)
+		operator_menu
 	end
 end
-			
+
+def passenger_menu
+	system('clear')
+	puts "*** PASSENGER MENU ***"
+	puts "Type [1] to view the stops along a train line."
+	puts "Type [2] to view a station location and the lines that stop there."
+	puts "To return to the maine menu please type [m]"
+	user_choice = gets.chomp
+	if user_choice == '1'
+		list_lines
+	elsif user_choice == '2'
+		list_stations
+	elsif user_choice == 'm'
+		welcome
+	else
+		puts "Sorry, that wasn't a valid option."
+		sleep(1.0)
+		passenger_menu	
+	end	
+end
+
+def add_station
+	system('clear')
+	puts "*** ADD A TRAIN STATION ***"
+	puts "Please type the station name:"
+	station_name = gets.chomp
+	new_station = Station.new({:name => station_name})
+	new_station.save
+	puts "\n"
+	puts "'#{new_station.name}' has been added!"
+	sleep(1.0)
+	operator_menu
+end
+
+def delete_station
+end
+
+def list_stations
+	puts "*** TRAIN STATIONS ***"
+	Station.all.each do |station|
+		puts "#{station.name}"
+	end
+	puts "\n"
+	puts "Type [1] to delete a station."
+	puts "Type [2] to return to the operator menu."
+	puts "Type [3] to return to the main menu."
+	user_choice = gets.chomp
+	if user_choice == '1'
+		delete_station
+	elsif user_choice == '2'
+		operator_menu
+	elsif user_choice == '3'
+		welcome
+	else 
+		puts "Sorry, that wasn't a valid option."
+		sleep(1.0)
+		list_stations
+	end	
+end
+
+def add_line
+	system('clear')
+	puts "*** ADD A TRAIN LINE ***"
+	puts "Please type the train line name:"
+	line_name = gets.chomp
+	new_line = Line.new({:name => line_name})
+	new_line.save
+	puts "\n"
+	puts "'#{new_line.name}' has been added!"
+	line_stop
+end	
+
+def line_stop
+	puts "\n"
+	puts "Where will this train line go?"
+	sleep(0.5)
+	puts "Select a stop from the list below and press enter."
+	puts "\n"
+	Station.all.each do |station|
+		puts "#{station.name}"
+	end
+	puts "\n"
+	station_name = gets.chomp
+	new_stop = Line.stops(new_line, station_name)
+	puts "You have assigned the '#{new_line}' line to stop at '#{station_name}' Station!"
+	puts "\n"
+	puts "Type [1] to add another stop."
+	puts "Type [2] to return to the operator menu."
+	user_choice = gets.chomp
+	if user_choice == '1'
+		add_stop
+	elsif user_choice == '2'
+		operator_menu
+	else 
+		puts "Sorry, that wasn't a valid option."
+		sleep(1.0)
+		add_stop
+	end	
+end 			
 welcome
 		
